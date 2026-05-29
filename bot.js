99aeeed33db445e2af620bcff0214397dd6571c2
Kaka2002 <Kaka2002@users.noreply.github.com>
2026-05-29 15:01:28 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,15 +13,11 @@ app.listen(PORT, () => console.log(`Monitor na porta ${PORT}`));
 //const client = new Client({puppeteer: {executablePath: 'C:/Program Files/Google/Chrome/Application/chrome.exe',}});
 const client = new Client({
     puppeteer: {
-        headless: true,
+        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || undefined,
         args: [
-            '--no-sandbox',
+            '--no-sandbox', 
             '--disable-setuid-sandbox',
             '--disable-dev-shm-usage',
-            '--disable-accelerated-2d-canvas',
-            '--no-first-run',
-            '--no-zygote',
-            '--single-process', // Ajuda a economizar memória no Render
             '--disable-gpu'
         ],
     }
